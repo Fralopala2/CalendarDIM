9d14762a1404d4bed94543c41fe565dc5bb228db
Paco Lopez Alarte <spanocrypt@gmail.com>
2026-01-07 21:20:49 +0100

    Add files via upload
    Carga inicial proyecto

---
+++ calendario.sql
@@ -0,0 +1,63 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 17-12-2025 a las 18:04:25
+-- Versión del servidor: 10.4.19-MariaDB
+-- Versión de PHP: 7.4.20
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `calendario`
+--
+CREATE DATABASE IF NOT EXISTS `calendario` DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish_ci;
+USE `calendario`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `eventoscalendar`
+--
+
+CREATE TABLE `eventoscalendar` (
+  `id` int(11) NOT NULL,
+  `evento` varchar(250) DEFAULT NULL,
+  `color_evento` varchar(20) DEFAULT NULL,
+  `fecha_inicio` varchar(20) DEFAULT NULL,
+  `fecha_fin` varchar(20) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `eventoscalendar`
+--
+ALTER TABLE `eventoscalendar`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `eventoscalendar`
+--
+ALTER TABLE `eventoscalendar`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
